3c8b26191d253f337422efe072ae85de36ece103
Pascal Thelke <p.thelke@googlemail.com>
2024-05-24 18:25:43 +0200

    implmented all inputs for the dialog

--- simple-crm/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simple-crm/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,29 +1,38 @@
-import {Component, Inject} from '@angular/core';
+import { Component } from '@angular/core';
 import {
-  MatDialog,
-  MAT_DIALOG_DATA,
-  MatDialogRef,
   MatDialogTitle,
   MatDialogContent,
   MatDialogActions,
   MatDialogClose,
 } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { ExampleHeader } from '../example-header/example-header.component';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogActions,MatDialogContent,MatFormFieldModule,FormsModule,MatDialogClose],
+  imports: [
+    MatDialogActions,
+    MatDialogContent,
+    MatFormFieldModule,
+    FormsModule,
+    MatDialogTitle,
+    MatButtonModule,
+    MatDialogClose,
+    MatInputModule,
+    MatDatepickerModule,
+    ExampleHeader
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class DialogAddUserComponent {
-
-
-  onNoClick(){
-    
-  }
+  onNoClick() {}
+  exampleHeader = ExampleHeader;
 }
