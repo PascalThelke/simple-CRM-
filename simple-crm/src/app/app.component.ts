a601c92cbced634fc07c226fab137b78c19b9855
Pascal Thelke <p.thelke@googlemail.com>
2024-07-06 05:05:33 +0200

    setted database to store client data

--- simple-crm/src/app/app.component.ts
+++ simple-crm/src/app/app.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+} from 'firebase/firestore/lite';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simple-crm';
-  
-  constructor(private router: Router) {}
+
+  constructor(private router: Router) {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBtdpKP8KQpyQ15K8StzNo5Gly2NxrWFys',
+      authDomain: 'simple-crm-23a6e.firebaseapp.com',
+      projectId: 'simple-crm-23a6e',
+      storageBucket: 'simple-crm-23a6e.appspot.com',
+      messagingSenderId: '657982237885',
+      appId: '1:657982237885:web:f990e8265f489175c75bd4',
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+  }
 
   navigateTo(path: string) {
     this.router.navigate([path]);
   }
-
 }
